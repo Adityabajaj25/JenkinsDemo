c4d1c4036b86f0fa1015c0760c60876553cf1d65
Aditya Bajaj <robinsingh21@aol.com>
2023-08-14 18:12:58 +0530

    first commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestClass {
+    WebDriver driver;
+    @BeforeClass()
+    public void Setup(){
+        System.setProperty("webdriver.chrome.driver", "D:\\Aditya\\test\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void testcase1(){
+        System.out.println("This is test case 1");
+    }
+
+    @Test
+    public void testcase2() throws InterruptedException {
+        Thread.sleep(5000);
+        System.out.println("This is test case 2");
+    }
+
+    @AfterClass
+    public void teardown(){
+        driver.close();
+    }
+}
